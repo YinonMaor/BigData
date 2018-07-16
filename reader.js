66960cc40aabace2ea86043c75ec761ebfbb1244
Yinon <yinonmail@gmail.com>
2018-07-16 12:08:24 +0300

    added working prediction

--- reader.js
+++ reader.js
@@ -1,21 +1,7 @@
 const csv = require("csvtojson");
 
 module.exports = {
-    readCsvFile(filePath) {
-        return new Promise(resolve => {
-            const players = []
-            csv
-            .fromPath(filePath)
-            .on("data", data => {
-                players.push(data)
-            })
-            .on("end", () => {
-                console.log("done reading data from csv")
-                resolve(players)
-            })
-        })
-    },
-    read(csvFilePath) {
+    readCsvFile(csvFilePath) {
         return new Promise(resolve => {
             csv()
             .fromFile(csvFilePath)
