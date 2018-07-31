dd5fe1400b2fd7aef2412014d4dfb4ccaa53ad4e
Yinon Cohen <yinonc@wix.com>
2018-07-31 20:58:59 +0300

    wip

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const WebHDFS = require('webhdfs');
 module.exports = WebHDFS.createClient({
-  user: 'maorshabtay',
+  user: 'hadoop-docker',
   host: 'http://localhost',
   path: '/',
   port: 50070
