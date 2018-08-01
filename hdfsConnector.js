bcd77dfecfc44a892a620970cba5533a2d1bb097
Yinon Cohen <yinonc@wix.com>
2018-08-01 11:02:15 +0300

    wip

--- hdfsConnector.js
+++ hdfsConnector.js
@@ -4,11 +4,14 @@ const hdfs = require('./client');
 //const WebHDFS = require('webhdfs');
 //const hdfs = WebHDFS.createClient();
 
+console.log('0')
 const localDataFile = fs.createReadStream(path.join(__dirname, 'PlayerPersonalDataM.csv'));
-
+console.log('1')
 const remoteDataFile = hdfs.createWriteStream('/data/PlayerPersonalDataM.csv');
-
+console.log('2')
 localDataFile.pipe(remoteDataFile);
+
+console.log('3')
 
 remoteDataFile.on('error', err => {
     console.log(err);
